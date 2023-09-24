b9bb2fad27edfebf57de129e5c58b13487394a40
imdad Adelabou <imdadadelabou0@gmail.com>
2023-09-24 12:52:06 +0200

    [FEAT] Woring on onboarding view

---
+++ lib/models/onboarding/onboarding.model.dart
@@ -0,0 +1,11 @@
+class Onboarding {
+  final String title;
+  final String description;
+  final String urlImg;
+
+  const Onboarding({
+    required this.title,
+    required this.description,
+    required this.urlImg,
+  });
+}
---
+++ lib/utils/constant.dart
@@ -0,0 +1,23 @@
+//Onboarding sample content
+import 'package:easy_trip/models/onboarding/onboarding.model.dart';
+
+List<Onboarding> onBoardingFr = const [
+  Onboarding(
+    title: "Explorez des circuits populaires",
+    description:
+        "Parcourez une sélection de circuit passionnants créés par des voyageurs comme vous",
+    urlImg: "",
+  ),
+  Onboarding(
+    title: "Interagissez avec d'autres voyageurs ",
+    description:
+        "Connectez-vous avec des personnes partageant les mêmes intérêts et partagez vos expériences de voyage",
+    urlImg: "",
+  ),
+  Onboarding(
+    title: "Partagez Votre Savoir-Faire",
+    description:
+        "Faites partie de la communauté de voyageurs passionnés. Partagez vos connaissances locales, découvertes cachées et conseils pratiques avec d'autres voyageurs en créant des itinéraires authentiques",
+    urlImg: "",
+  ),
+];
