0478b9d22f08c0d65be146208a35be92eadb0b4b
imdad Adelabou <imdadadelabou@imdads-MacBook-Pro.local>
2024-05-21 00:16:46 +0200

    [FEAT] moving the current route GoRoute

--- lib/views/home/traveller/home_traveller.dart
+++ lib/views/home/traveller/home_traveller.dart
@@ -1,20 +1,9 @@
 import "dart:io";
 
-import "package:easy_trip/components/circuit_card.dart";
-import "package:easy_trip/components/creator_card.dart";
 import "package:easy_trip/components/custom_bottom_bar.dart";
-import "package:easy_trip/components/head_title_section.dart";
-import "package:easy_trip/components/icon_notification.dart";
-import "package:easy_trip/models/circuit/circuit.dart";
-import "package:easy_trip/models/creator/creator.dart";
-import "package:easy_trip/models/path/path.dart";
-import "package:easy_trip/utils/app_asset.dart";
-import "package:easy_trip/utils/app_message.dart";
 import "package:easy_trip/utils/constant.dart";
 import "package:easy_trip/utils/styles.dart";
 import "package:flutter/material.dart";
-import "package:gap/gap.dart";
-import "package:google_fonts/google_fonts.dart";
 
 class HomeTraveller extends StatefulWidget {
   static String routeName = "/home-traveller";
